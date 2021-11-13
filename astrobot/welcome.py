944a6e31d7beb78382c6362bd49b5a36dbf43b8d
kevinshome <noah.tanner7@gmail.com>
2021-11-13 10:41:57 -0500

    minor changes

--- astrobot/welcome.py
+++ astrobot/welcome.py
@@ -6,14 +6,15 @@ class WelcomeWagon(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot: commands.Bot = bot
         self.welcome_channel = 904815153177063514
+        self.channel_ids = [None, None, None] # GUIDELINES, ROLES, GENERAL
     
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         title = f"Welcome to {member.guild}!"
         description = f"\
-Visit GUIDELINES_CHANNEL_ID to view the server rules.\n\
-Then, check out ROLES_CHANNEL_ID to get yourself some fresh roles.\n\
-Finally, head on over to GENERAL_CHANNEL_ID and join in the conversation!"
+Visit {self.channel_ids[0]} to view the server rules.\n\
+Then, check out {self.channel_ids[1]} to get yourself some fresh roles.\n\
+Finally, head on over to {self.channel_ids[2]} and join in the conversation!"
         embed = discord.Embed(
             title=title,
             description=description,
