9f86f5a2368e0659a5674a5746b76f718fd3f11b
kevinshome <noah.tanner7@gmail.com>
2021-11-27 12:39:51 -0500

    make MochjiColor inheritence-based and change each method to @classmethod // NO VERSION BUMP

--- astrobot/colors.py
+++ astrobot/colors.py
@@ -1,20 +1,28 @@
 import discord
 
-class MochjiColor:
-    def black():
-        return discord.Colour.from_rgb(24, 24, 24)
-    def red():
-        return discord.Colour.from_rgb(201, 54, 80)
-    def orange():
-        return discord.Colour.from_rgb(221, 129, 54)
-    def yellow():
-        return discord.Colour.from_rgb(221, 200, 98)
-    def green():
-        return discord.Colour.from_rgb(118, 197, 104)
-    def blue():
-        return discord.Colour.from_rgb(59, 121, 190)
-    def purple():
-        return discord.Colour.from_rgb(134, 76, 194)
-    def white():
-        return discord.Colour.from_rgb(217, 217, 217)
+class MochjiColor(discord.Colour):
+    @classmethod
+    def black(c):
+        return c.from_rgb(24, 24, 24)
+    @classmethod
+    def red(c):
+        return c.from_rgb(201, 54, 80)
+    @classmethod
+    def orange(c):
+        return c.from_rgb(221, 129, 54)
+    @classmethod
+    def yellow(c):
+        return c.from_rgb(221, 200, 98)
+    @classmethod
+    def green(c):
+        return c.from_rgb(118, 197, 104)
+    @classmethod
+    def blue(c):
+        return c.from_rgb(59, 121, 190)
+    @classmethod
+    def purple(c):
+        return c.from_rgb(134, 76, 194)
+    @classmethod
+    def white(c):
+        return c.from_rgb(217, 217, 217)
     
